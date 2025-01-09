9b02b03cbc7193a201b75f2115a41059b7552c8d
NepUWU <125133224+DeveloperPrajwalYT@users.noreply.github.com>
2025-01-09 17:14:48 +0545

    Update and rename hyx.lua (1).txt to terminal.lua

---
+++ terminal.lua
@@ -0,0 +1,308 @@
+-- NepXSenjha on top!!
+--[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ 
+local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
+
+-- NepXSenjha on top!!
+local Window = OrionLib:MakeWindow({
+    Name = "HYxDEATH HUB",
+        --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ 
+    HidePremium = false,
+    IntroText = "NepXScriptVerse",
+    SaveConfig = true,
+    ConfigFolder = "ScriptVerse"
+})
+
+-- NepXSenjha on top!!
+local BloxFruitsTab = Window:MakeTab({
+    Name = "Blox Fruits",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+-- NepXSenjha on top!!
+BloxFruitsTab:AddButton({
+    Name = "RedZ Hub",
+        --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ 
+    Callback = function()
+        loadstring(game:HttpGet("https://rawscripts.net/raw/Blox-Fruits-Redz-hub-aim-bot-23823", true))()
+        print("RedZ Hub loaded.")
+    end
+})
+
+BloxFruitsTab:AddButton({
+    Name = "Cokka Hub",
+        --[ Credits to senjha.termx for the source code ]--
+ 
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/UserDevEthical/Loadstring/main/CokkaHub.lua"))()
+        print("Cokka Hub loaded.")
+    end
+})
+
+BloxFruitsTab:AddButton({
+    Name = "HoHo Hub",
+    Callback = function()
+            --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ 
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI"))()
+        print("HoHo Hub loaded.")
+    end
+})
+--[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ 
+BloxFruitsTab:AddButton({
+    Name = "Banana Hub",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Nghia11n/Banana-Hub/main/bananahub.lua"))()
+        print("Banana Hub loaded.")
+    end
+})
+--[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ 
+
+-- NepXSenjha on top!!
+local FischTab = Window:MakeTab({
+    Name = "Fisch",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+-- NepXSenjha on top!!
+FischTab:AddButton({
+    Name = "Neuron X Auto Fish",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/NeuronX/FischScripts/main/AutoFish.lua"))()
+        print("Neuron X Auto Fish script loaded.")
+    end
+        --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ 
+})
+
+FischTab:AddButton({
+    Name = "NaokiHub Auto Fish",
+    Callback = function()
+            --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ 
+        loadstring(game:HttpGet("https://naokihub.vercel.app", true))()
+        print("NaokiHub Auto Fish script loaded.")
+    end
+})
+
+FischTab:AddButton({
+    Name = "Moon X Auto Farm",
+    Callback = function()
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/cba17b913ee63c7bfdbb9301e2d87c8b.lua"))()
+        print("Moon X Auto Farm script loaded.")
+    end
+})
+
+FischTab:AddButton({
+    Name = "Ronix Hub",
+    Callback = function()
+        --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/361c351458df79e1e4022531dd10f0e3.lua"))()
+        print("Ronix Hub script loaded.")
+    end
+})
+
+-- NepXSenjha on top!!
+local TSBTab = Window:MakeTab({
+    Name = "The Strongest Battlegrounds",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+-- NepXSenjha on top!!
+TSBTab:AddButton({
+    Name = "Spooks Hub TSB Script",
+    Callback = function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/ApparentlySpooks/spookshubTSB/main/SHTSB.txt'))()
+        print("Spooks Hub TSB Script loaded.")
+    end
+})
+
+TSBTab:AddButton({
+    Name = "Forge Hub TSB Script",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Skzuppy/forge-hub/main/loader.lua"))()
+        print("Forge Hub TSB Script loaded.")
+    end
+})
+
+TSBTab:AddButton({
+    Name = "Speed Hub X TSB Script",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+        print("Speed Hub X TSB Script loaded.")
+    end
+})
+--[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ --[ Credits to senjha.termx for the source code ]--
+ 
+TSBTab:AddButton({
+    Name = "Mautiku's TSB Hack",
+    Callback = function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/Mautiku/ehh/main/strong%20guest.lua.txt'))()
+        print("Mautiku's TSB Hack loaded.")
+    end
+})
+
+-- NepXSenjha on top!! 
+OrionLib:Init()
